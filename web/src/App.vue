8eb4070d1b3abcf57f52e7156bd6593adfa0216a
潘朝思 <panchaosi@bytedance.com>
2021-01-13 22:34:20 +0800

    feat: add framework

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,17 +1,23 @@
-
 <template>
-    <div id="App">
-      <Index/>
-    </div>
+  <div id="app">
+      <router-view></router-view>
+  </div>
 </template>
 
 <script>
-import Index from './components/Index'
 
 export default {
-  name: 'App',
-  components: {
-    Index
-  }
+  name: 'app',
 }
 </script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  /* margin-top: 60px; */
+}
+</style>
