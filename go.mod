3c061f330b0f79d5a66504ae704f4234fe78839b
潘朝思 <panchaosi@bytedance.com>
2021-01-27 03:03:49 +0800

    ci: add workflow

--- go.mod
+++ go.mod
@@ -8,7 +8,9 @@ require (
 	github.com/gin-contrib/static v0.0.0-20200916080430-d45d9a37d28e
 	github.com/gin-gonic/gin v1.6.3
 	github.com/go-playground/validator/v10 v10.2.0
-	github.com/pbnjay/memory v0.0.0-20201129165224-b12e5d931931
+	github.com/lestrrat-go/file-rotatelogs v2.4.0+incompatible
+	github.com/lestrrat-go/strftime v1.0.4 // indirect
+	github.com/rifflock/lfshook v0.0.0-20180920164130-b9218ef580f5
 	github.com/robfig/cron/v3 v3.0.1
 	github.com/sirupsen/logrus v1.7.0
 	github.com/spf13/viper v1.7.1
--- internal/pkg/conf/init.go
+++ internal/pkg/conf/init.go
@@ -5,8 +5,11 @@ import (
 	"flag"
 	"fmt"
 	"github.com/fsnotify/fsnotify"
-	log "github.com/sirupsen/logrus"
+	"github.com/lestrrat-go/file-rotatelogs"
+	"github.com/rifflock/lfshook"
+	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
+	"time"
 )
 
 var (
@@ -16,6 +19,7 @@ var (
 func Init(ctx context.Context) (err error) {
 	flag.Parse()
 
+	//viper.SetDefault("log.path", "/tmp/daily_problem.log")
 	viper.SetConfigFile(*conf)
 	if err := viper.ReadInConfig(); err != nil {
 		return fmt.Errorf("failed to read config, path: %s, err: %+v", *conf, err)
@@ -23,8 +27,37 @@ func Init(ctx context.Context) (err error) {
 
 	viper.WatchConfig()
 	viper.OnConfigChange(func(e fsnotify.Event) {
-		log.Infof("config file changed: %s", e.Name)
+		logrus.Infof("config file changed: %s", e.Name)
 	})
+
+	//if err := initLogger(); err != nil {
+	//	return err
+	//}
+
+	return nil
+}
+
+func initLogger() error {
+
+	path := viper.GetString("log.path")
+
+	writer, err := rotatelogs.New(
+		path+".%Y%m%d%H",
+		rotatelogs.WithLinkName("path"),
+		rotatelogs.WithMaxAge(24*time.Hour),
+		rotatelogs.WithRotationTime(time.Hour),
+	)
+	if err != nil {
+		return fmt.Errorf("init logger failed: %+v", err)
+	}
+
+	logrus.New().Hooks.Add(lfshook.NewHook(
+		lfshook.WriterMap{
+			logrus.InfoLevel:  writer,
+			logrus.ErrorLevel: writer,
+		},
+		&logrus.JSONFormatter{},
+	))
 
 	return nil
 }
