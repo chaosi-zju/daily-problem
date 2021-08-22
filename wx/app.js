d291e6e0c7e53a3bf63c2d6e37442e8a48b697bc
潘朝思 <panchaosi@bytedance.com>
2021-08-22 17:53:10 +0800

    fix: wx share

--- wx/app.js
+++ wx/app.js
@@ -12,20 +12,27 @@ App({
       }
     })
     //显示转发分享
-    wx.showShareMenu()
+    wx.showShareMenu({
+      menus: ['shareAppMessage', 'shareTimeline'],
+      success(res) {
+        util.toast(wx, '转发成功', 800)
+      },
+      fail(e) {
+        util.toast(wx, '转发失败', 800)
+      }
+    })
   },
-  shareAppMessage: function() {
-    //转发
+  onShareAppMessage(){
     return {
       title: '快来一起刷题吧，每天三道算法题，面试不是问题！',
       path: 'pages/index/index',
-      imageUrl: '/image/fan.jpg',
-      success: function(res) {
-        util.toast(wx, '转发成功', 800)
-      },
-      fail: function(res) {
-        util.toast(wx, '转发失败', 800)
-      }
+      // imageUrl: '/image/fan.jpg',
     }
   },
+  onShareTimeline(){
+    return {
+      title: '快来一起刷题吧，每天三道算法题，面试不是问题！',
+      // imageUrl: '/image/fan.jpg',
+    }
+  }
 })
