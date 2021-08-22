40f440758281835d7e91a5303c9a232d25685d1d
chaosi-zju <chaosi@zju.edu.cn>
2021-08-22 15:56:54 +0800

    feat: add another markdown plugin

--- wx/app.js
+++ wx/app.js
@@ -3,6 +3,7 @@ App({
   globalData: {
     problems: []
   },
+  towxml: require('/towxml/index'),
   onLaunch() {
     // 登录
     wx.login({
@@ -10,5 +11,21 @@ App({
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
       }
     })
-  }
+    //显示转发分享
+    wx.showShareMenu()
+  },
+  shareAppMessage: function() {
+    //转发
+    return {
+      title: '快来一起刷题吧，每天三道算法题，面试不是问题！',
+      path: 'pages/index/index',
+      imageUrl: '/image/fan.jpg',
+      success: function(res) {
+        util.toast(wx, '转发成功', 800)
+      },
+      fail: function(res) {
+        util.toast(wx, '转发失败', 800)
+      }
+    }
+  },
 })
--- wx/pages/problem/problem.js
+++ wx/pages/problem/problem.js
@@ -3,13 +3,16 @@ const app = getApp()
 
 Page({
   data: {
-    article: {}
+    markmode: 0, // choose markdown plugin: 0.wemark; 1.towxml 
+    article: {},
   },
   onLoad: function (options) {
+    this.setData({markmode: options.idx % 2})
     let p = app.globalData.problems[options.idx]
     let content = "## " + p.name + "\n[OJ链接](" + p.link + ")\n\n" + p.content + "\n\n### 解答\n" + p.result
-    this.setData({
-      article: content
-    })
+    if (this.data.markmode != 0) {
+      content = app.towxml(content, 'markdown', {})
+    }
+    this.setData({article: content})
   }
 })
